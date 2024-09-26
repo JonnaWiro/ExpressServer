e4b4ed95f40b82e9d2e4d98986fb898e76748635
Jonna Wiro <jonna.wiro@student.careeria.fi>
2024-09-26 12:42:58 +0300

    Lisättiin pin-koodi kirjautuminen

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Express.js palvelin //
 const path = require('path')
 const express = require('express')
+const fs = require('fs').promises
 
 const app = express()
 
@@ -9,6 +10,21 @@ const herkut = require('./herkut.json')
 //GET ALL etsitään kaikki herkut jsonista
 app.get('/api/herkut', (req, res) => {
     res.json(herkut)
+})
+
+//-------------------Pinkoodin lukeminen txt tiedostosta palvelimelta ja lähettäminen selaimelle---------------------
+
+app.get('/api/getpin', async (req, res) => {
+    try {
+        // Read the content of the text file
+        const savedPin = await fs.readFile('./pin.txt', 'utf-8')
+
+        // Send the file content as the response
+        res.send(savedPin)
+    } catch (error) {
+        console.error('Error reading file:', error)
+        res.status(500).send('Internal Server Error')
+    }
 })
 
 //Tehdään polkumääritys public -kansioon
